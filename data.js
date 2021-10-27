fa8b4a6eb800e41b0018db1dd482061db2e4964c
Lotte Koblens <lotte.koblens@hva.nl>
2021-10-27 12:32:13 +0200

    split en lowercase

--- data.js
+++ data.js
@@ -4,9 +4,8 @@ function dataOpschonen() {
     .then(json => {
       console.log(json);
 
-      // console.log(json[2]['Wat is je favoriete windrichting?']);
-
-      function huisdierKort(zin) {
+      // functie waarmee de vraag wordt omgezet in alleen een woord 'huisdier'
+      function huisdierSchoon(zin) {
         const new_key = 'huisdier';
         const huisdieren = [];
 
@@ -21,9 +20,26 @@ function dataOpschonen() {
             huisdieren.push(json[i].huisdier)
           }
         }
-        console.log(huisdieren);
+        console.log(huisdieren[0]);
+
+        // split zodat wanneer er hond of hamster is ingevuld, er alleen hond over blijft
+        const new_huisdieren = [];
+        for (let i = 0; i < json.length; i++) {
+          const split = huisdieren[i].split(" ", 1);
+          new_huisdieren.push(split[0]);
+        }
+        console.log(new_huisdieren);
+
+        // zet alle antwoorden om naar lowercase
+        const new_huisdieren_lowercase = [];
+        for (let i = 0; i < json.length; i++) {
+          const lowercase = new_huisdieren[i].toLowerCase();
+          new_huisdieren_lowercase.push(lowercase);
+        }
+        console.log(new_huisdieren_lowercase);
+
       }
-      huisdierKort('Wat is je favoriete soort huisdier?');
+      huisdierSchoon('Wat is je favoriete soort huisdier?');
     })
 }
 
