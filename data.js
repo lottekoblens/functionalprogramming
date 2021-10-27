fc1d050c034c32cecb732ee9bed2f50e570f7fd4
Lotte Koblens <lotte.koblens@goeiezaak.com>
2021-10-27 10:48:10 +0200

    applied functional programming

--- data.js
+++ data.js
@@ -1,29 +1,30 @@
-fetch("data.json")
-  .then(response => response.json())
-  .then(json => {
-    console.log(json);
+function dataOpschonen() {
+  fetch("data.json")
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
 
-    // console.log(json[2]['Wat is je favoriete windrichting?']);
+      // console.log(json[2]['Wat is je favoriete windrichting?']);
 
-    const objecten = Object.keys(json);
-    const old_key = 'Wat is je favoriete windrichting?';
-    const new_key = 'windrichting';
-    const windrichtingen = [];
+      function huisdierKort(zin) {
+        const new_key = 'huisdier';
+        const huisdieren = [];
 
-    for (let i = 0; i < json.length; i++) {
-      if (old_key !== new_key) {
-        Object.defineProperty(
-          json[i],
-          new_key,
-          Object.getOwnPropertyDescriptor(json[i], old_key)
-        );
-        delete json[i][old_key];
-        console.log(json[i].windrichting);
+        for (let i = 0; i < json.length; i++) {
+          if (zin !== new_key) {
+            Object.defineProperty(
+              json[i],
+              new_key,
+              Object.getOwnPropertyDescriptor(json[i], zin)
+            );
+            delete json[i][zin];
+            huisdieren.push(json[i].huisdier)
+          }
+        }
+        console.log(huisdieren);
       }
-    }
-    for (let i = 0; i < json.length; i++) {
-      windrichtingen.push(json[i].windrichting)
-    }
+      huisdierKort('Wat is je favoriete soort huisdier?');
+    })
+}
 
-  })
-
+dataOpschonen();
