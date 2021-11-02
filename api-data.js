7fb484b5f770b5690f56dc769b978ef171ee76b6
Lotte Koblens <lotte.koblens@hva.nl>
2021-11-02 13:45:50 +0100

    function changeKey works

--- api-data.js
+++ api-data.js
@@ -1,15 +1,15 @@
 function dataOphalen() {
+    //fetch om data uit API op te halen
     fetch("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=netherlands&api_key=f2ab12a57fcca396592451123c0c3ba1&format=json")
         .then(response => response.json())
         .then(data => {
+            // geef data mee aan de functies
             EverythingToLowerCase(data)
             deleteUnusedData(data)
             changeKey(data)
-            // console.log(Object.keys(data.tracks.track[0]))
-            // console.log(data)
-            // console.log(data.tracks.track[0].name)
             return data
         }).then(cleanedData => {
+            // console.log opgeschoonde data
             console.log(cleanedData)
         })
 }
@@ -21,33 +21,22 @@ function EverythingToLowerCase(data) {
 }
 
 function deleteUnusedData(data) {
+    // gebruik forEach om te loopen over array track. Vervolgens verwijder ik de keys met hun value die ik niet nodig heb
     data.tracks.track.forEach(track => {
         delete track.image;
         delete track.mbid;
         delete track.streamable;
-        return track;
     });
 }
 
 function changeKey(data) {
-    data.tracks.track.map(track => {
-        Object.keys(track).map(track => {
-            if (track.name !== "nameSong") {
-                track.name = "nameSong"
-            }
-            else {
-                console.log('Key is al juist')
-            }
-        })
+    // met behulp van een forEach verander ik de key van name in de array track naar nameSong
+    data.tracks.track.forEach(track => {
+       Object.defineProperty(track, 'nameSong', Object.getOwnPropertyDescriptor(track, 'name'));
+        delete track.name;
     })
+    console.log(data.tracks.track)
 }
 
 
 dataOphalen();
-
-
-        // if (old_key !== new_key) {
-        //     Object.defineProperty(o, new_key,
-        //         Object.getOwnPropertyDescriptor(o, old_key));
-        //     delete o[old_key];
-        // }
