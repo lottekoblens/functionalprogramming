e32d1c6191bb51dcf997274fe4df0dc82cb5a69d
Lotte Koblens <lotte.koblens@hva.nl>
2021-11-02 13:52:10 +0100

    changed functions to arrow functions

--- api-data.js
+++ api-data.js
@@ -1,4 +1,4 @@
-function dataOphalen() {
+const dataOphalen = () => {
     //fetch om data uit API op te halen
     fetch("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=netherlands&api_key=f2ab12a57fcca396592451123c0c3ba1&format=json")
         .then(response => response.json())
@@ -14,13 +14,13 @@ function dataOphalen() {
         })
 }
 
-function EverythingToLowerCase(data) {
+const EverythingToLowerCase = data => {
     for (let i = 0; i < data.tracks.track.length; i++) {
         data.tracks.track[i].name = data.tracks.track[i].name.toLowerCase();
     }
 }
 
-function deleteUnusedData(data) {
+const deleteUnusedData = data => {
     // gebruik forEach om te loopen over array track. Vervolgens verwijder ik de keys met hun value die ik niet nodig heb
     data.tracks.track.forEach(track => {
         delete track.image;
@@ -29,7 +29,7 @@ function deleteUnusedData(data) {
     });
 }
 
-function changeKey(data) {
+const changeKey = data => {
     // met behulp van een forEach verander ik de key van name in de array track naar nameSong
     data.tracks.track.forEach(track => {
        Object.defineProperty(track, 'nameSong', Object.getOwnPropertyDescriptor(track, 'name'));
